bc03f8c0af8cfadf6e0019addb3240409577fc32
Patrick Fournier <p_fournier@hushmail.com>
2023-03-10 16:57:30 -0500

    Implement class Toeplitz.

--- src/Toeplitz.jl
+++ src/Toeplitz.jl
@@ -16,21 +16,35 @@ end
 BitToeplitz(vr, vc) = BitToeplitz(vcat(reverse(vc), vr),
                                   length(vc) + 1, length(vr))
 
-### Abstract Array Interface
+#######################
+# Toeplitz definition #
+#######################
+
+struct Toeplitz{T} <:AbstractToeplitz{T}
+    data::Vector{T}
+    nrows::Int
+    ncols::Int
+end
+
+Toeplitz(vr, vc) = Toeplitz(vcat(reverse(vc), vr), length(vc) + 1, length(vr))
+
+############################
+# Abstract Array Interface #
+############################
+
 import Base: size, getindex, setindex!, IndexStyle
 
 @generated IndexStyle(::AbstractToeplitz) = IndexCartesian()
 
-size(M::BitToeplitz) = (M.nrows, M.ncols)
+size(M::AbstractToeplitz) = (M.nrows, M.ncols)
 
 compute_idx(row, col, vc_length) = vc_length + col - row + 1
 
-getindex(M::BitToeplitz, row, col) =
+getindex(M::AbstractToeplitz, row, col) =
     M.data[compute_idx(row, col, size(M, 1) - 1)]
 
-setindex!(M::BitToeplitz, v, row, col) =
-    setindex!(M.data, v, compute_idx(row, col, size(M, 1) - 1))
+getindex(M::AbstractToeplitz, row, ::Colon) =
+    M.data[range(compute_idx(row, 1, size(M, 1) - 1), length = M.ncols)]
 
-#################################
-# End of BitToeplitz definition #
-#################################
+setindex!(M::AbstractToeplitz, v, row, col) =
+    setindex!(M.data, v, compute_idx(row, col, size(M, 1) - 1))
